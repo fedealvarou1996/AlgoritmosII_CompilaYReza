06d5a99602d29215aecc1e74bf767ba685cc8206
Federico Alvarez Rouco <fedealvarou@gmail.com>
2026-03-31 21:00:04 -0300

    Primer commit

---
+++ Pilas estaticas/src/Main.java
@@ -0,0 +1,67 @@
+import Implementacion.Estrategia_1;
+import Implementacion.Estrategia_1_String;
+import Implementacion.Estrategia_2;
+import Implementacion.Estrategia_3;
+import Interfaces.PilaTDA;
+import Interfaces.PilaTDAStrings;
+
+public class Main {
+    public static void main(String[] args) {
+
+        PilaTDA p1 = new Estrategia_1();
+        PilaTDA p2 = new Estrategia_2();
+        PilaTDA p3 = new Estrategia_3();
+        PilaTDAStrings p4 = new Estrategia_1_String();
+
+        probarPila("Estrategia 1", p1);
+        probarPila("Estrategia 2", p2);
+        probarPila("Estrategia 3", p3);
+        probarPilaString("Estrategia 4", p4);
+    }
+
+    public static void probarPila(String nombre, PilaTDA pila) {
+        System.out.println("=== " + nombre + " ===");
+
+        pila.InicializarPila();
+
+        pila.Apilar(10);
+        pila.Apilar(20);
+        pila.Apilar(30);
+
+        System.out.println("Tope luego de apilar 10, 20, 30: " + pila.Tope());
+
+        pila.Desapilar();
+        System.out.println("Tope luego de desapilar una vez: " + pila.Tope());
+
+        pila.Desapilar();
+        System.out.println("Tope luego de desapilar otra vez: " + pila.Tope());
+
+        pila.Desapilar();
+        System.out.println("¿Pila vacía?: " + pila.PilaVacia());
+
+        System.out.println();
+    }
+
+    public static void probarPilaString(String nombre, PilaTDAStrings pilaString) {
+        System.out.println("=== " + nombre + " ===");
+
+        pilaString.InicializarPila();
+
+        pilaString.Apilar("fi.uba.ar");
+        pilaString.Apilar("campus.utn.edu.ar");
+        pilaString.Apilar("stackoverflow.com");
+
+        System.out.println("Tope luego de apilar 'stackoverflow.com', 'campus.utn.edu.ar', fi.uba.ar: " + pilaString.Tope());
+
+        pilaString.Desapilar();
+        System.out.println("Tope luego de desapilar una vez: " + pilaString.Tope());
+
+        pilaString.Desapilar();
+        System.out.println("Tope luego de desapilar otra vez: " + pilaString.Tope());
+
+        pilaString.Desapilar();
+        System.out.println("¿Pila vacía?: " + pilaString.PilaVacia());
+
+        System.out.println();
+    }
+}
